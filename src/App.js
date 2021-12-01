50644b26e36d054b62f8c8ebb668cf257dd9f985
waterhydroxyl <waterhydroxyl@gmail.com>
2021-12-01 15:57:05 +0800

    ci: redux初始化

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { memo } from 'react';
 import { renderRoutes } from 'react-router-config';
 import { HashRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import routes from '@/router';
 
 import YLAppHeader from '@/components/app-header';
@@ -9,12 +12,12 @@ import YLAppFooter from '@/components/app-footer';
 
 export default memo(function App() {
   return (
-    <div>
+    <Provider store={store}>
       <HashRouter>
         <YLAppHeader />
         {renderRoutes(routes)}
         <YLAppFooter />
       </HashRouter>
-    </div>
+    </Provider>
   );
 });
